050543d38bf0fbb38919ad112a06fd5877cd86c0
Kitkikat <Nastpenyaz@ya.ru>
2024-07-16 14:27:44 +0300

    Update DictionaryService.java

--- src/main/java/com/example/dictionary/service/DictionaryService.java
+++ src/main/java/com/example/dictionary/service/DictionaryService.java
@@ -1,16 +1,45 @@
 package com.example.dictionary.service;
 
 import com.example.dictionary.model.Dictionary;
+import com.example.dictionary.repository.DictionaryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@AllArgsConstructor
+public class DictionaryService {
+    private final DictionaryRepository dictionaryRepository;
 
-public interface DictionaryService {
+    public Dictionary findDictionaryById(Long id) {
+        return dictionaryRepository.findById(id).orElse(null);
+    }
 
-    Dictionary saveDictionary(Dictionary dictionary);
-    Dictionary findDictionaryById(Long id);
-    Dictionary updateDictionary(Long id, Dictionary dictionary);
-    Dictionary deleteDictionary(Long id);
-    List<Dictionary> findAllDictionary();
+    public List<Dictionary> findAllDictionary() {
+        return dictionaryRepository.findAll();
+    }
 
+    public Dictionary saveDictionary(Dictionary dictionary) {
+        return dictionaryRepository.save(dictionary);
+    }
+
+    public Dictionary updateDictionary(Long id, Dictionary dictionary) {
+        Dictionary existingDictionary = dictionaryRepository.findById(id).orElse(null);
+        if (existingDictionary != null) {
+            existingDictionary.setCode(dictionary.getCode());
+            existingDictionary.setDescription(dictionary.getDescription());
+            return dictionaryRepository.save(existingDictionary);
+        }
+        return null;
+    }
+
+    public boolean deleteDictionary(Long id) {
+        Dictionary dictionary = dictionaryRepository.findById(id).orElse(null);
+        if (dictionary != null) {
+            dictionaryRepository.delete(dictionary);
+            return true;
+        }
+        return false;
+    }
 }
