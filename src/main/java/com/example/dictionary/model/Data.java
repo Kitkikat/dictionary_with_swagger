b99ec7cb444d296ab1f9b3dba01f8a9b7fb181d4
Kitkikat <Nastpenyaz@ya.ru>
2024-07-16 14:26:14 +0300

    Update Data.java

--- src/main/java/com/example/dictionary/model/Data.java
+++ src/main/java/com/example/dictionary/model/Data.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import jakarta.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -14,34 +15,41 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
 @Table(name = "data")
 public class Data {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     @Column(name = "id", nullable = false, unique = true)
-
     @Schema(hidden = true)
-
     private Long id;
 
-    @Column(name = "dictionary_id", nullable = false)
-
+    @ManyToOne
+    @JoinColumn(name = "dictionary_id", referencedColumnName = "id")
     @Schema(name = "dictionary_id", example = "1111", required = true)
-
-    private Long dictionary_id;
+    private Dictionary dictionary;
 
     @Column(name = "code")
-
     @Schema(name = "code", example = "test", required = false)
-
     private String code;
 
     @Column(name = "value", nullable = false)
-
     @Schema(name = "value", example = "test", required = true)
-
     private String value;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Dictionary getDictionary() {
+        return dictionary;
+    }
+
+    public void setDictionary(Dictionary dictionary) {
+        this.dictionary = dictionary;
+    }
 }
