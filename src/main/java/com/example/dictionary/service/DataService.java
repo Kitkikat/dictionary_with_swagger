52d1ec4756aaf191d841d0bd06eb08b569621436
Kitkikat <Nastpenyaz@ya.ru>
2024-07-16 14:27:08 +0300

    Update DataService.java

--- src/main/java/com/example/dictionary/service/DataService.java
+++ src/main/java/com/example/dictionary/service/DataService.java
@@ -2,18 +2,42 @@ package com.example.dictionary.service;
 
 import com.example.dictionary.model.Data;
 import com.example.dictionary.repository.DataRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface DataService {
+@Service
+@AllArgsConstructor
+public class DataService {
+    private final DataRepository dataRepository;
 
-    Data saveData(Data data);
-    Data findDataById(Long id);
-    List<Data> findAllData();
-    Data updateData(Long id, Data data);
-    Data deleteData(Long id);
+    public Optional<Data> findDataById(Long id) {
+        return dataRepository.findById(id);
+    }
 
+    public List<Data> findAllData() {
+        return dataRepository.findAll();
+    }
+
+    public Data saveData(Data data) {
+        return dataRepository.save(data);
+    }
+
+    public Optional<Data> updateData(Long id, Data data) {
+        return dataRepository.findById(id).map(existingData -> {
+            existingData.setCode(data.getCode());
+            existingData.setValue(data.getValue());
+            existingData.setDictionary(data.getDictionary());
+            return dataRepository.save(existingData);
+        });
+    }
+
+    public boolean deleteData(Long id) {
+        return dataRepository.findById(id).map(data -> {
+            dataRepository.delete(data);
+            return true;
+        }).orElse(false);
+    }
 }
