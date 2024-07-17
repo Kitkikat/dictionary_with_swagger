1c7fa033a7a4916ee7dc54a8238f967a8316d3d0
Kitkikat <Nastpenyaz@ya.ru>
2024-07-17 13:52:41 +0300

    Update DataService.java

--- src/main/java/com/example/dictionary/service/DataService.java
+++ src/main/java/com/example/dictionary/service/DataService.java
@@ -1,43 +1,60 @@
 package com.example.dictionary.service;
 
 import com.example.dictionary.model.Data;
+import com.example.dictionary.model.Dictionary;
 import com.example.dictionary.repository.DataRepository;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
-@AllArgsConstructor
+@Transactional
 public class DataService {
+
     private final DataRepository dataRepository;
+    private final DictionaryService dictionaryService;
 
-    public Optional<Data> findDataById(Long id) {
-        return dataRepository.findById(id);
-    }
-
-    public List<Data> findAllData() {
-        return dataRepository.findAll();
+    public DataService(DataRepository dataRepository, DictionaryService dictionaryService) {
+        this.dataRepository = dataRepository;
+        this.dictionaryService = dictionaryService;
     }
 
     public Data saveData(Data data) {
+        // Если Dictionary уже существует, установите его
+        Dictionary dictionary = data.getDictionary();
+        if (dictionary != null && dictionary.getId() != null) {
+            dictionary = dictionaryService.findDictionaryById(dictionary.getId());
+            if (dictionary != null) {
+                data.setDictionary(dictionary);
+            }
+        }
         return dataRepository.save(data);
     }
 
-    public Optional<Data> updateData(Long id, Data data) {
-        return dataRepository.findById(id).map(existingData -> {
-            existingData.setCode(data.getCode());
-            existingData.setValue(data.getValue());
-            existingData.setDictionary(data.getDictionary());
-            return dataRepository.save(existingData);
+    public Optional<Data> findDataById(UUID id) {
+        return dataRepository.findById(id);
+    }
+
+    public Optional<Data> updateData(UUID id, Data newData) {
+        return dataRepository.findById(id).map(data -> {
+            data.setDictionary(newData.getDictionary());
+            data.setCode(newData.getCode());
+            data.setValue(newData.getValue());
+            return dataRepository.save(data);
         });
     }
 
-    public boolean deleteData(Long id) {
+    public boolean deleteData(UUID id) {
         return dataRepository.findById(id).map(data -> {
             dataRepository.delete(data);
             return true;
         }).orElse(false);
+    }
+
+    public List<Data> findAllData() {
+        return dataRepository.findAll();
     }
 }
