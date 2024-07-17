50639dabded74ee7085d1de3855952fc1c6e2a41
Kitkikat <Nastpenyaz@ya.ru>
2024-07-17 13:52:02 +0300

    Update DataRepository.java

--- src/main/java/com/example/dictionary/repository/DataRepository.java
+++ src/main/java/com/example/dictionary/repository/DataRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 @Repository
-public interface DataRepository extends JpaRepository<Data, Long> {
+public interface DataRepository extends JpaRepository<Data, UUID> {
 }
